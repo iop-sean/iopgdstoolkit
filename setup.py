aff0695614930dbb52bb15119674ad27a284a93d
iop-sean <77781869+iop-sean@users.noreply.github.com>
2023-04-11 10:50:24 +0100

    removed.git from url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='iopgdstoolkit',
     version='0.0.1',
     packages=[''],
-    url='https://github.com/iop-sean/iopgdstoolkit.git',
+    url='https://github.com/iop-sean/iopgdstoolkit',
     license='',
     author='Sean Bommer',
     author_email='sean.bommer.2014@uni.strath.ac.uk',
