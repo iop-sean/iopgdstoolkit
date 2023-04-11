e54904a6d28a61e9eaa1593b1a8ec936973b3428
iop-sean <77781869+iop-sean@users.noreply.github.com>
2023-04-11 10:26:33 +0100

    add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='iopgdstoolkit',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/iop-sean/iopgdstoolkit.git',
+    license='',
+    author='Sean Bommer',
+    author_email='sean.bommer.2014@uni.strath.ac.uk',
+    description='Code for generating suspended membranes'
+)
